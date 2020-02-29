bc891954637e525359e3ea727f5382cc938c5060
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-02-29 02:53:29 -0300

    Library Pages updated

---
+++ src/pages/Edit_Library_Item/index.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import api from '../../services/api'
+
+import './styles.css'
+
+
+export default function EditLibraryItem( { history } ){
+
+    const { id } = useParams()
+
+    const [ loadingState, setLoadingState ] = useState(false)
+
+    const [ type, setType ] = useState('')
+    const [ file, setFile ] = useState(null)
+    const [ link, setLink ] = useState('')
+    const [ name, setName ] = useState('')
+
+    useEffect(()=> {
+        async function loadActivities(){
+
+            const response = await api.get('/library/' + id)
+
+            setType(response.data.type)
+            setLink(response.data.location)
+            setName(response.data.name)      
+        }
+
+        loadActivities()
+    }, [id] )
+
+    async function handleSubmit(event){
+
+        setLoadingState(true)
+
+        event.preventDefault()
+
+        let data = new FormData()
+
+        data.set("file", file)
+        data.set("name", name)
+        data.set("link", link)
+        
+        await api.post('/library/edit/' + id, data)
+
+        history.push('/library/' + type)
+    }
+
+    return (
+        
+        <form onSubmit={handleSubmit}>
+            
+            <label htmlFor="statement" >Nome *</label>
+            <input id='name'
+            placeholder='Nome do conteúdo'
+            value={name}
+            onChange = {event => setName(event.target.value)}
+            />
+
+            <label htmlFor="link" hidden={ type === 'book' } >Link do conteúdo *</label>
+            <input id='link'
+            placeholder='Link do Youtube'
+            value={link}
+            onChange = {event => setLink(event.target.value)}
+            hidden={ type === 'book' }
+            />
+
+            <label htmlFor="file" hidden={ type !== 'book' } >Arquivo PDF</label>
+            <a id='btn'className="btn" href={link} target='_blank'rel="noopener noreferrer" hidden={ type !== 'book' } > Ver arquivo atual</a>
+            <input type="file"  onChange={event => setFile(event.target.files[0])} hidden={ type !== 'book' } />
+
+
+            <button type ='submit' className="btn" disabled= { loadingState ? true : false } >
+                { loadingState ? "Atualizando . . ."  : "Atualizar" }
+            </button>
+        </form>
+    )
+}
--- src/pages/Library_List/index.js
+++ src/pages/Library_List/index.js
@@ -1,36 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import api from "../../services/api"
 
 import './styles.css'
 
 export default function LibraryList(){
-    const [library, setLibrary] = useState([])
-    
-    useEffect(()=> {
+    const [ library, setLibrary ] = useState([])
+    const { type } = useParams()
+
+    async function deleteItem(id){
+
+        await api.delete('/library/delete/' + id)
+
+        window.location.reload()
+        
+    }
+
+    useEffect(( )=> {
         async function loadActivities(){
-            const response = await api.get('/library')
+
+            const response = await api.post('/library/view/' + type)
 
             setLibrary(response.data)           
         }
 
         loadActivities()
-    }, [] )
+    }, [type] )
   
     
     return (
-<>
+        <>
+            <h1>Biblioteca</h1>
+            <Link to={"/library/new/" + type}><button className='btn'>Cadastrar novo item</button></Link>
             <ul className= 'library-list' id="library">
                 {library.map(library_item => (
                     <li key={library_item._id}>
-                        <header style = {{
-                            backgroundImage: `url(${library_item.location})` }}></header>
-                        <strong>{library_item.word}</strong>
+                        <strong>{library_item.name}</strong>
+                        <p>   
+                            <Link to={"/library/edit/" + library_item._id}><button className='btn' id='editar'>editar</button></Link>
+                            <button className='btn' id='excluir' onClick={event => deleteItem(library_item._id)}>excluir</button>
+                        </p>
                     </li>
                 ))}
             </ul>
 
-            <Link to={"/library/new"}><button className='btn'>Cadastrar novo item</button></Link>
         </>
     )
 }

6d3d73cbe235efa30853a372872235baa94645ec
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-02-29 02:51:37 -0300

    Routes updated

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
 
 import Home from './pages/Home'
-import ActivityList from './pages/Activity_List'
 import DictionaryList from './pages/Dictionary_List'
 import LibraryList from './pages/Library_List'
 import ModuleList from './pages/Module_List'
@@ -14,6 +13,10 @@ import NewLibraryItem from './pages/New_Library_Item'
 import NewModule from './pages/New_Module'
 import NewObjective from './pages/New_Objective'
 import NewSubjective from './pages/New_Subjective'
+import EditDictionaryItem from './pages/Edit_Dictionary_Item'
+import EditLibraryItem from './pages/Edit_Library_Item'
+import EditModule from './pages/Edit_Module'
+import ViewModule from './pages/View_Module'
 
 export default function Routes(){
     return (
@@ -21,17 +24,26 @@ export default function Routes(){
 
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/activities/:id'exact component={ActivityList} />
-          <Route path='/activities/:id/new' exact component={NewActivity} />
+          <Route path='/activities/new/:id' exact component={NewActivity} />
           <Route path='/subjective/new/:id' exact component={NewSubjective} />
           <Route path='/objective/new/:id' exact component={NewObjective} />
           <Route path='/objective/alternative/:id'component={NewAlternative} />
+          
+
           <Route path='/dictionary'exact component={DictionaryList} />
+          <Route path='/dictionary/edit/:id'exact component={EditDictionaryItem} />
           <Route path='/dictionary/new'component={NewDictionaryItem} />
+
+
           <Route path='/module'exact component={ModuleList} />
           <Route path='/module/new'component={NewModule} />
-          <Route path='/library'exact component={LibraryList} />
-          <Route path='/library/new'component={NewLibraryItem} />
+          <Route path='/module/edit/:id'exact component={EditModule} />
+          <Route path='/module/view/:id'exact component={ViewModule} />
+
+          
+          <Route path='/library/new/:type'component={NewLibraryItem} />
+          <Route path='/library/edit/:id'component={EditLibraryItem} />
+          <Route path='/library/:type'exact component={LibraryList} />
         </Switch>
 
       </BrowserRouter>

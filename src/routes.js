b1628a2e67ec2409d8ff8b61e676a5d0936514ec
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-03-02 03:36:33 -0300

    Routes updated

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
-
-import Home from './pages/Home'
 import DictionaryList from './pages/Dictionary_List'
 import LibraryList from './pages/Library_List'
 import ModuleList from './pages/Module_List'
 import NewActivity from './pages/New_Activity'
-import NewAlternative from './pages/New_Alternative'
+import EditActivity from './pages/Edit_Activity'
 import NewDictionaryItem from './pages/New_Dictionary_Item'
 import NewLibraryItem from './pages/New_Library_Item'
 import NewModule from './pages/New_Module'
-import NewObjective from './pages/New_Objective'
-import NewSubjective from './pages/New_Subjective'
 import EditDictionaryItem from './pages/Edit_Dictionary_Item'
 import EditLibraryItem from './pages/Edit_Library_Item'
 import EditModule from './pages/Edit_Module'
@@ -23,11 +19,9 @@ export default function Routes(){
       <BrowserRouter>
 
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={ModuleList} />
           <Route path='/activities/new/:id' exact component={NewActivity} />
-          <Route path='/subjective/new/:id' exact component={NewSubjective} />
-          <Route path='/objective/new/:id' exact component={NewObjective} />
-          <Route path='/objective/alternative/:id'component={NewAlternative} />
+          <Route path='/activity/edit/:id'component={EditActivity} />
           
 
           <Route path='/dictionary'exact component={DictionaryList} />

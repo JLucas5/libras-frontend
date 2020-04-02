b617d82dc413a501c225669288574c55fa556ecc
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-04-02 17:41:49 -0300

    Files are removable on editing now

--- src/pages/Edit_Module/index.js
+++ src/pages/Edit_Module/index.js
@@ -44,6 +44,15 @@ export default function EditModule( { history } ){
         [image]
     )
 
+    function deleteImage(){
+        setImage(null)
+        setOld_image(null)
+    }
+
+    function deletePDF(){
+        setFile(null)
+        setOld_file(null)
+    }
     async function handleSubmit(event){
 
         setLoadingState(true)
@@ -68,7 +77,7 @@ export default function EditModule( { history } ){
     return (
         <form onSubmit={handleSubmit}>
             <h1>Editando Módulo</h1>
-        
+
             <label htmlFor="name">TÍTULO</label>
             <input id='name'
             placeholder='O nome do seu módulo'
@@ -98,10 +107,12 @@ export default function EditModule( { history } ){
             <input type="file" onChange={event => setImage(event.target.files[0])} />
             <img src={camera} alt="Select img"/>
             </label>
+            <button onClick={event => deleteImage()} type="button" className="remove-button">Remover imagem</button>
 
             <label htmlFor="file" >Arquivo PDF</label>
-            <a id='btn'className="btn" href={old_file} target='_blank'rel="noopener noreferrer" hidden={ !old_file } >Ver arquivo antigo</a>
+            <a id='btn'className="btn" href={old_file} target='_blank'rel="noopener noreferrer" hidden={ !old_file } >Ver PDF antigo</a>
             <input type="file"  onChange={event => setFile(event.target.files[0])}/>
+            <button onClick={event => deletePDF()} type="button" className="remove-button">Remover PDF</button>
 
             <button type ='submit' className="btn" disabled= { loadingState ? true : false } >
                 { loadingState ? "Salvando . . ." : "Salvar" }

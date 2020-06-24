d977d2e95004c706936bc2cb9075b5113af419de
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-06-24 13:18:36 -0300

    Login feature added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React from 'react'
-import './App.css'
-import Routes from './routes'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { NavDropdown } from 'react-bootstrap'
+import React, { useContext } from "react"
+import "./App.css"
+import Routes from "./routes"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "react-bootstrap/Navbar"
+import app from "./base"
+import Nav from "react-bootstrap/Nav"
+import { NavDropdown } from "react-bootstrap"
 
 function App() {
 	return (
 		<div>
-			<Navbar sticky='top' collapseOnSelect expand='lg'>
-				<Navbar.Brand href='/home'>Painel EduLibras</Navbar.Brand>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-				<Navbar.Collapse id='responsive-navbar-nav'>
-					<Nav.Link href='/module'>Módulos</Nav.Link>
-					<Nav.Link href='/dictionary'>Dicionário</Nav.Link>
-					<NavDropdown title='Biblioteca'>
-						<NavDropdown.Item href='/library/video'>
+			<Navbar sticky="top" collapseOnSelect expand="lg">
+				<Navbar.Brand href="/home">Painel EduLibras</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav.Link href="/module">Módulos</Nav.Link>
+					<Nav.Link href="/dictionary">Dicionário</Nav.Link>
+					<NavDropdown title="Biblioteca">
+						<NavDropdown.Item href="/library/video">
 							Videos
 						</NavDropdown.Item>
-						<NavDropdown.Item href='/library/music'>
+						<NavDropdown.Item href="/library/music">
 							Músicas
 						</NavDropdown.Item>
-						<NavDropdown.Item href='/library/book'>
+						<NavDropdown.Item href="/library/book">
 							Livros
 						</NavDropdown.Item>
 					</NavDropdown>
-					<Nav.Link href='/aboutus'>Sobre nós</Nav.Link>
+					<Nav.Link href="/aboutus">Sobre nós</Nav.Link>
+					<Nav.Link
+						className="exit-button"
+						onClick={() => {
+							app.auth().signOut()
+						}}>
+						Sair
+					</Nav.Link>
 				</Navbar.Collapse>
 			</Navbar>
 
-			<div className='container'>
-				<div className='content'>
+			<div className="container">
+				<div className="content">
 					<Routes />
 				</div>
 			</div>
---
+++ src/pages/Login/index.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react"
+import firebase from "firebase"
+import { withRouter, Redirect } from "react-router"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import app from "../../base"
+import { AuthContext } from "../../Auth"
+
+import "./Login.css"
+
+const Login = () => {
+	const { currentUser } = useContext(AuthContext)
+
+	const uiConfig = {
+		signInFlow: "popup",
+		signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+	}
+
+	if (currentUser) {
+		return <Redirect to="/module" />
+	}
+
+	return (
+		<div className="Login-form">
+			<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={app.auth()} />
+		</div>
+	)
+}
+
+export default withRouter(Login)

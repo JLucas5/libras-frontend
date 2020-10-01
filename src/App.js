28c54315e3a85ca81bed75806525ca1d12aacc90
BuildTools <unconfigured@null.spigotmc.org>
2020-09-30 21:51:23 -0300

    Remove NavBar do App e cria como componente

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import React, { useContext } from "react"
-import "./App.css"
-import Routes from "./routes"
-import "bootstrap/dist/css/bootstrap.min.css"
-import Navbar from "react-bootstrap/Navbar"
-import app from "./base"
-import Nav from "react-bootstrap/Nav"
-import { NavDropdown } from "react-bootstrap"
+import React, { useContext } from 'react'
+import './App.css'
+import Routes from './routes'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBarSuperior from './components/NavBarSuperior'
 
-function App() {
+export default function App() {
+
+	const user = React.createContext();
+
 	return (
-		<div>
-			<Navbar sticky="top" collapseOnSelect expand="lg">
-				<Navbar.Brand href="/home">Painel EduLibras</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav.Link href="/module">Módulos</Nav.Link>
-					<Nav.Link href="/dictionary">Dicionário</Nav.Link>
-					<NavDropdown title="Biblioteca">
-						<NavDropdown.Item href="/library/video">
-							Videos
-						</NavDropdown.Item>
-						<NavDropdown.Item href="/library/music">
-							Músicas
-						</NavDropdown.Item>
-						<NavDropdown.Item href="/library/book">
-							Livros
-						</NavDropdown.Item>
-					</NavDropdown>
-					<Nav.Link href="/aboutus">Sobre nós</Nav.Link>
-					<Nav.Link
-						className="exit-button"
-						onClick={() => {
-							app.auth().signOut()
-						}}>
-						Sair
-					</Nav.Link>
-				</Navbar.Collapse>
-			</Navbar>
-
-			<div className="container">
-				<div className="content">
+		<>		
+			<NavBarSuperior />
+			<div className='container'>
+				<div className='content'>
 					<Routes />
 				</div>
 			</div>
-		</div>
-	)
+		</>
+	)	
 }
-
-export default App
